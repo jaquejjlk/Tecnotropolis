8439373db65bd7d3fcbdf6f470a6468e2635e922
Jaqueline <jaqueline_fanton@estudante.sesisenai.org.br>
2026-04-27 21:42:16 -0300

    J

--- tecnotropolis/src/app/app.routes.ts
+++ tecnotropolis/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Perfil } from './pages/perfil/perfil';
+import { Contato } from './pages/contato/contato';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'perfil',component:Perfil},
+     {path: 'contato',component:Contato}
+];
